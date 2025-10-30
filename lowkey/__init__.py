b75cfe058e541403d8831b250e237e1d8eb06805
draew6 <adosko6@gmail.com>
2025-10-30 22:49:23 +0100

    imports

--- lowkey/__init__.py
+++ lowkey/__init__.py
@@ -12,3 +12,4 @@ from .models.user import User
 from .utils import generate_run_id
 from .errors import ParsingError
 from .parser import Parser, HTMLFile, JSONFile
+from .components.context import BeautifulSoupCrawlingContext, ParsedHttpCrawlingContext
